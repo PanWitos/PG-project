6689f7d1c92721e772e306dca07200fb95908c3b
SzyKania <72377599+SzyKania@users.noreply.github.com>
2023-06-14 18:22:06 +0200

    last fix

--- Controllers/TurnQueue.gd
+++ Controllers/TurnQueue.gd
@@ -5,9 +5,7 @@ class_name TurnQueue
 var active_character
 
 func initialize():
-	var fighters = get_children() #TODO
-#	for fighter in fighters:
-#		print(fighter.statistics.statistics["Agility"])
+	var fighters = get_children()
 	fighters.sort_custom(self, 'sort_fighters')
 	for fighter in fighters:
 		fighter.raise()
@@ -19,6 +17,4 @@ func play_turn():
 	active_character = get_child(new_index)
 
 static func sort_fighters(a : Sprite, b : Sprite) -> bool:
-	print(a)
-	print(b)
 	return a.statistics.statistics["Agility"] > b.statistics.statistics["Agility"]
